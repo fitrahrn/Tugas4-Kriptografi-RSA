10541eff4b5e272134316ac1cc7103d9a063b317
ARomygithub <ahmadromyzahran@gmail.com>
2024-04-07 21:23:20 +0700

    paket B,C,D

--- part_A.py
+++ part_A.py
@@ -1,4 +1,50 @@
 import socket
+from decimal import *
+from Crypto.Util.number import *
+
+getcontext().prec = 700
+
+def find_cube_root(x):
+    l = 1
+    r = int(Decimal(x).sqrt())
+    ret = -1
+    while (l<=r):
+        mid = (l+r)//2
+        if(mid**3 <= x):
+            ret = mid
+            l = mid+1
+        else:
+            r = mid-1
+    return ret
+
+def solve(paket_soal, n, e, c):
+    if(paket_soal=='A'):
+        pass
+    elif (paket_soal=='B'):
+        p = int(Decimal(n).sqrt())
+        # print(p)
+        # print(isPrime(p))
+        tot = (p)*(p-1)
+        d = pow(e, -1, tot)
+        m_int = pow(c, d, n)
+        m_asli = long_to_bytes(m_int).decode() 
+        return m_asli
+    elif (paket_soal=='C'):
+        for d in range(2**15,(2**16)+1):
+            try:
+                m_int = pow(c,d,n)
+                m_asli = long_to_bytes(m_int).decode()
+                if(m_asli.startswith("KRIPTOGRAFIITB{")):
+                    return m_asli
+            except Exception as e:
+                continue
+    elif (paket_soal=='D'):
+        m_int = find_cube_root(c)
+        m_asli = long_to_bytes(m_int).decode()
+        return m_asli
+    elif (paket_soal=='E'):
+        pass
+    return "0"
 
 def netcat(hn, p):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
